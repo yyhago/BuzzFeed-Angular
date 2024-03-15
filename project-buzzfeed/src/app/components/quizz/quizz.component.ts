e7c4b1ecd6c5e5a4d79386671ea83c82f710f7d5
yyhago <yhago.felipe.teles@gmail.com>
2024-03-15 16:11:53 -0300

    Finalização do projeto

--- project-buzzfeed/src/app/components/quizz/quizz.component.ts
+++ project-buzzfeed/src/app/components/quizz/quizz.component.ts
@@ -42,17 +42,35 @@ export class QuizzComponent implements OnInit {
 
   playerChoose(value:string){
     this.answers.push(value)
+    this.nextStep()
   }
 
-  async nestStop(){
+
+  async nextStep(){
     this.questionIndex+=1
 
     if(this.questionMaxIndex > this.questionIndex){
       this.questionSelected = this.questions[this.questionIndex]
     }else{
+      const finalAnswers:string = await this.checkResult(this.answers)
       this.finished = true
+      this.answersSelected = quizz_questions.results[finalAnswers as keyof typeof quizz_questions.results]
     }
+  }
 
+  async checkResult(answers:string[]){
+
+    const result = answers.reduce((previous, current, i, arr)=>{
+      if(
+        arr.filter(item => item === previous).length >
+        arr.filter(item => item === current).length
+      ){
+        return previous
+      } else{
+        return current
+      }
+    })
+    return result
   }
 
 }
