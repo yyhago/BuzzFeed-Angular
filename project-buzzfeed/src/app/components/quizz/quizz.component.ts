d1be8c1c95c892469635badbbd189cee6192786f
yyhago <yhago.felipe.teles@gmail.com>
2024-03-15 15:21:22 -0300

    Lógica das regras de jogo funcionando

--- project-buzzfeed/src/app/components/quizz/quizz.component.ts
+++ project-buzzfeed/src/app/components/quizz/quizz.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { NgIf } from '@angular/common';
+import { NgIf, NgFor } from '@angular/common';
+import quizz_questions from '../../../assets/data/quizz_questions.json'
 
 @Component({
   selector: 'app-quizz',
   standalone: true,
-  imports: [NgIf],
+  imports: [NgIf, NgFor],
   templateUrl: './quizz.component.html',
   styleUrl: './quizz.component.css'
 })
@@ -12,7 +13,7 @@ export class QuizzComponent implements OnInit {
 
   title:string = ""
 
-  question:any
+  questions:any
   questionSelected:any = {} // Correção do erro no console resolvido com as chaves, garantindo que nao seja undefined
 
   answers:string[] = []
@@ -25,7 +26,33 @@ export class QuizzComponent implements OnInit {
 
   constructor(){}
   ngOnInit(): void {
-      
+      if(quizz_questions){
+        this.finished = false
+        this.title = quizz_questions.title
+
+        this.questions = quizz_questions.questions
+        this.questionSelected = this.questions[this.questionIndex]
+
+        this.questionIndex = 0
+        this.questionMaxIndex = this.questions.length
+
+      }
+  }
+
+
+  playerChoose(value:string){
+    this.answers.push(value)
+  }
+
+  async nestStop(){
+    this.questionIndex+=1
+
+    if(this.questionMaxIndex > this.questionIndex){
+      this.questionSelected = this.questions[this.questionIndex]
+    }else{
+      this.finished = true
+    }
+
   }
 
 }
