e0d1101ac3ed408b205e081f049c26eb3c80076d
yyhago <yhago.felipe.teles@gmail.com>
2024-03-15 14:51:03 -0300

    Criando as propriedades lógicas

--- project-buzzfeed/src/app/components/quizz/quizz.component.ts
+++ project-buzzfeed/src/app/components/quizz/quizz.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-quizz',
   standalone: true,
-  imports: [],
+  imports: [NgIf],
   templateUrl: './quizz.component.html',
   styleUrl: './quizz.component.css'
 })
-export class QuizzComponent {
+export class QuizzComponent implements OnInit {
+
+  title:string = ""
+
+  question:any
+  questionSelected:any = {} // Correção do erro no console resolvido com as chaves, garantindo que nao seja undefined
+
+  answers:string[] = []
+  answersSelected:string = ""
+
+  questionIndex:number = 0
+  questionMaxIndex:number = 0
+
+  finished:boolean = false
+
+  constructor(){}
+  ngOnInit(): void {
+      
+  }
 
 }
